9991bbb115f64b2723c9547e94552fa926df2758
Siddharth <sid.kalantri9@gmail.com>
2026-01-11 23:02:49 +0530

    fix: match port 3000 and start telegram

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ import "./bots/telegram.js";
 dotenv.config();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
-const port = process.env.PORT || 8080;
+
+// Use Port 3000 to match your Azure settings
+const port = process.env.PORT || 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
